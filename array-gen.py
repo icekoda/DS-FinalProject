372c8d228975869a81643c9a0f81ab02ab8d28da
icekoda <imaclooser@gmail.com>
2021-03-29 22:45:53 -0400

    Updated array-gen.py

--- array-gen.py
+++ array-gen.py
@@ -1,18 +1,22 @@
 #array-gen.py
-#IMPORTS
-
+##IMPORTS
+#
 import barcode
 from barcode import EAN13
 from barcode.writer import ImageWriter
 
-from PIL import Image
+from PIL import Image, ImageOps
 from numpy import asarray
+import numpy as np
+#
+##IMPORTS
 
-#IMPORTS
-
-
-image = Image.open('MNIST_DS/0/img_10007.jpg')
-
-numpydata = asarray(image)
-
-print(numpydata.shape)
+#Set image = image that will be opened
+image = Image.open('MNIST_DS/1/img_1000.jpg').convert('L')
+img = Image.open('MNIST_DS/1/img_1000.jpg')
+#Converts the gray image to an array
+data = np.asarray(image)
+newdata = np.asarray(img)
+#Print the array
+print(data)
+print(newdata)
